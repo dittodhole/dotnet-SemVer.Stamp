488c96ffbb7554fddb595aa09b112742bcd8ccf3
Andreas Niedermair <andreas@niedermair.name>
2016-02-21 23:52:33 +0100

    added more properties to Configuration

--- Fody.SemVer/Configuration.cs
+++ Fody.SemVer/Configuration.cs
@@ -20,23 +20,62 @@ namespace Fody.SemVer
         throw new ArgumentNullException(nameof(element));
       }
 
-      var attribute = element.Attribute("UseProject"); // Not L10N
-      if (attribute == null)
       {
-        throw new WeavingException($"Unable to read UseProject from configuration");
+        var attribute = element.Attribute("UseProject"); // Not L10N
+        if (attribute != null)
+        {
+          try
+          {
+            this.UseProject = bool.Parse(attribute.Value);
+          }
+          catch (Exception exception)
+          {
+            throw new WeavingException($"Unable to parse {attribute.Value} as boolean from configuartion",
+                                       exception);
+          }
+        }
       }
 
-      try
       {
-        this.UseProject = bool.Parse(attribute.Value);
+        var attribute = element.Attribute("PatchFormat"); // Not L10N
+        if (attribute != null)
+        {
+          this.PatchFormat = attribute.Value;
+        }
+        if (string.IsNullOrEmpty(this.PatchFormat))
+        {
+          this.PatchFormat = @"^fix(\(.*\))*: ";
+        }
       }
-      catch (Exception exception)
+
       {
-        throw new WeavingException($"Unable to parse {attribute.Value} as boolean from configuartion",
-                                   exception);
+        var attribute = element.Attribute("FeatureFormat"); // Not L10N
+        if (attribute != null)
+        {
+          this.FeatureFormat = attribute.Value;
+        }
+        if (string.IsNullOrEmpty(this.FeatureFormat))
+        {
+          this.FeatureFormat = @"^feat(\(.*\))*: ";
+        }
+      }
+
+      {
+        var attribute = element.Attribute("BreakingChangeFormat"); // Not L10N
+        if (attribute != null)
+        {
+          this.BreakingChangeFormat = attribute.Value;
+        }
+        if (string.IsNullOrEmpty(this.BreakingChangeFormat))
+        {
+          this.BreakingChangeFormat = @"^perf(\(.*\))*: ";
+        }
       }
     }
 
     public bool UseProject { get; }
+    public string PatchFormat { get; }
+    public string FeatureFormat { get; }
+    public string BreakingChangeFormat { get; }
   }
 }
