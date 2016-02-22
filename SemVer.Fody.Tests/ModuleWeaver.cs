5b36f7b317188572ea5b0344092dcd223df92e97
Andreas Niedermair <andreas@niedermair.name>
2016-02-22 21:42:49 +0100

    added some tests

---
+++ SemVer.Fody.Tests/ModuleWeaver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SemVer.Fody
+{
+  public sealed partial class ModuleWeaver
+  {
+    private void Prerequisites()
+    {
+      throw new NotImplementedException();
+    }
+
+    private Version GetVersion(string repositoryPath,
+                               Version baseVersion,
+                               string patchFormat,
+                               string featureFormat,
+                               string breakingChangeFormat)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ SemVer.Fody.Tests/ModuleWeaverBaseTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SemVer.Fody.Tests
+{
+  [TestClass]
+  public class ModuleWeaverBaseTests
+  {
+    [TestMethod]
+    public void Version_Should_Be_Calculated_For_One_Fix()
+    {
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "fix: foobar"
+                                                },
+                                                "0.0.1");
+    }
+
+    [TestMethod]
+    public void Version_Should_Be_Calculated_For_Two_Fixes()
+    {
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "fix: foobar",
+                                                  "fix: foobar2"
+                                                },
+                                                "0.0.2");
+    }
+
+    [TestMethod]
+    public void Version_Should_Be_Calculated_For_One_Feature()
+    {
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "feat: foobar"
+                                                },
+                                                "0.1.0");
+    }
+
+    [TestMethod]
+    public void Version_Should_Be_Calculated_For_Two_Features()
+    {
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "feat: foobar",
+                                                  "feat: foobar2"
+                                                },
+                                                "0.2.0");
+    }
+
+    [TestMethod]
+    public void Version_Should_Be_Calculated_For_One_Breaking_Change()
+    {
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "perf: foobar"
+                                                },
+                                                "1.0.0");
+    }
+
+    [TestMethod]
+    public void Version_Should_Be_Calculated_For_Two_Breaking_Changes()
+    {
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "perf: foobar",
+                                                  "perf: foobar2"
+                                                },
+                                                "2.0.0");
+    }
+
+    [TestMethod]
+    public void Version_Should_Be_Calculated_For_Complex_Commits()
+    {
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "fix: foobar",
+                                                  "feat: foobar"
+                                                },
+                                                "0.1.0");
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "feat: foobar",
+                                                  "fix: foobar"
+                                                },
+                                                "0.1.1");
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "fix: foobar",
+                                                  "feat: foobar",
+                                                  "perf: foobar"
+                                                },
+                                                "1.0.0");
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "perf: foobar",
+                                                  "fix: foobar",
+                                                  "feat: foobar",
+                                                },
+                                                "1.1.0");
+      this.Get_Version_According_To_SemVer_Base(new[]
+                                                {
+                                                  "perf: foobar",
+                                                  "feat: foobar",
+                                                  "fix: foobar"
+                                                },
+                                                "1.1.1");
+    }
+
+    private void Get_Version_According_To_SemVer_Base(string[] commitMessages,
+                                                      string expectedVersion)
+    {
+      var moduleWeaver = new ModuleWeaver();
+      var baseVersion = new Version(0,
+                                    0,
+                                    0,
+                                    0);
+      var version = moduleWeaver.GetVersionAccordingToSemVer(commitMessages,
+                                                             baseVersion,
+                                                             @"^fix(\(.*\))*: ",
+                                                             @"^feat(\(.*\))*: ",
+                                                             @"^perf(\(.*\))*: ");
+
+      Assert.AreEqual(expectedVersion,
+                      version.ToString(3));
+    }
+  }
+}
