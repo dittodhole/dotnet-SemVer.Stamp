24596b67ff4c722389b6e3864c5a2ae49fe4bf25
Andreas Niedermair <andreas@niedermair.name>
2017-08-31 15:16:27 +0200

    annotated SemVerStampTask

--- src/SemVer.Git.MSBuild/SemVerStampTask.cs
+++ src/SemVer.Git.MSBuild/SemVerStampTask.cs
@@ -1,7 +1,9 @@
-﻿using SemVer.MSBuild;
+﻿using JetBrains.Annotations;
+using SemVer.MSBuild;
 
 namespace SemVer.Git.MSBuild
 {
+  [UsedImplicitly]
   public class SemVerStampTask : SemVerStampTaskBase
   {
     public override ICommitMessageProvider CreateCommitMessageProvider()
