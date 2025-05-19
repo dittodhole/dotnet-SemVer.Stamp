92f49e1d3a419f9ab086fcfa5966ea62bde5fa72
Andreas Niedermair <me+code@a76a6a65.nexus>
2025-05-19 12:52:09 +0200

    init

---
+++ src/SemVer.Git.MSBuild/GetVersion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using LibGit2Sharp;
+using NDepend.Path;
+using SemVer.MSBuild;
+
+namespace SemVer.Git.MSBuild
+{
+  public sealed class GetVersion : GetVersionBase
+  {
+    /// <inheritdoc />
+    protected override string[] GetCommitMessages()
+    {
+      var repositoryPath = Repository.Discover(this.SourcePath) ?? throw new InvalidOperationException($"Could not find repository for '{this.SourcePath}'");
+      var commonPath = GetVersion.GetCommonPath(repositoryPath,
+                                                this.SourcePath) ?? throw new InvalidOperationException($"'{this.SourcePath}' has no common path with '{repositoryPath}'");
+
+      string relativePath;
+      try
+      {
+        relativePath = this.SourcePath.Substring(commonPath.Length);
+      }
+      catch (Exception exception)
+      {
+        throw new InvalidOperationException($"Could not get {nameof(relativePath)}",
+                                            exception);
+      }
+
+      if (!string.IsNullOrEmpty(relativePath))
+      {
+        relativePath = relativePath.Replace("\\",
+                                            "/");
+      }
+
+      string[] result;
+      using (var repository = new Repository(repositoryPath))
+      {
+        // Unfortunately, CommitSortStrategies.Time | CommitSortStrategies.Reverse is not
+        // supported, hence we are ordering the LINQ-way afterwards ... #smh
+        var commitFilter = new CommitFilter
+                           {
+                             IncludeReachableFrom = repository.Head,
+                             ExcludeReachableFrom = this.BaseRevision,
+                             SortBy = CommitSortStrategies.Time
+                           };
+
+        IEnumerable<Commit> commits;
+        if (string.IsNullOrEmpty(relativePath))
+        {
+          commits = repository.Commits.QueryBy(commitFilter);
+        }
+        else
+        {
+          commits = repository.Commits.QueryBy(relativePath,
+                                               commitFilter)
+                              .Select(arg => arg.Commit);
+        }
+
+        result = commits.OrderBy(arg => arg.Author.When)
+                        .Select(arg => arg.Message)
+                        .ToArray();
+      }
+
+      return result;
+    }
+
+    [CanBeNull]
+    private static string GetCommonPath([NotNull] [ItemNotNull] params string[] paths)
+    {
+      paths.Select(arg => arg.ToAbsoluteDirectoryPath())
+           .ToArray()
+           .TryGetCommonRootDirectory(out var commonRootDirectory);
+
+      return commonRootDirectory?.ToString();
+    }
+  }
+}
---
+++ src/SemVer.Svn.MSBuild/GetVersion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using SemVer.MSBuild;
+using SharpSvn;
+
+namespace SemVer.Svn.MSBuild
+{
+  public sealed class GetVersion : GetVersionBase
+  {
+    /// <inheritdoc/>
+    protected override string[] GetCommitMessages()
+    {
+      Collection<SvnLogEventArgs> logItems;
+      using (var svnClient = new SvnClient())
+      {
+        SvnRevision start;
+        if (this.BaseRevision == null)
+        {
+          start = SvnRevision.Zero;
+        }
+        else
+        {
+          int startRevision;
+          try
+          {
+            startRevision = int.Parse(this.BaseRevision);
+          }
+          catch (Exception exception)
+          {
+            throw new InvalidOperationException($"Could not parse {nameof(this.BaseRevision)} '{this.BaseRevision}'",
+                                                exception);
+          }
+
+          start = startRevision;
+        }
+
+        if (!svnClient.GetLog(this.SourcePath,
+                              new SvnLogArgs
+                              {
+                                StrictNodeHistory = false,
+                                Range = new SvnRevisionRange(start,
+                                                             SvnRevision.Head)
+                              },
+                              out logItems))
+        {
+          throw new InvalidOperationException($"Could not get log for '{this.SourcePath}'");
+        }
+      }
+
+      var result = logItems.OrderBy(arg => arg.Revision)
+                           .Select(arg => arg.LogMessage)
+                           .ToArray();
+
+      return result;
+    }
+  }
+}
