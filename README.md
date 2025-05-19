c798f8361c771ebe336cd2c24509f3d1a7b8c6ac
Andreas Niedermair <me+code@a76a6a65.nexus>
2025-05-19 09:32:41 +0200

    init

---
+++ README.md
@@ -0,0 +1,73 @@
+![](assets/noun_60203_cc.png)
+
+# dotnet-SemVer.Stamp
+
+> Version your assemblies according to SemVer based on your VCS commit messages.
+
+## Build status
+
+[![](https://img.shields.io/appveyor/ci/dittodhole/dotnet-semver-stamp.svg)](https://ci.appveyor.com/project/dittodhole/dotnet-semver-stamp)
+
+## Installing
+
+### myget.org
+
+[![](https://img.shields.io/myget/dittodhole/vpre/SemVer.Git.MSBuild.svg)](https://www.myget.org/feed/dittodhole/package/nuget/SemVer.Git.MSBuild)
+
+```powershell
+PM> Install-Package -Id SemVer.Git.MSBuild -pre --source https://www.myget.org/F/dittodhole/api/v2
+```
+
+[![](https://img.shields.io/myget/dittodhole/vpre/SemVer.Svn.MSBuild.svg)](https://www.myget.org/feed/dittodhole/package/nuget/SemVer.Svn.MSBuild)
+
+```powershell
+PM> Install-Package -Id SemVer.Svn.MSBuild -pre --source https://www.myget.org/F/dittodhole/api/v2
+```
+
+### nuget.org
+
+[![](https://img.shields.io/nuget/v/SemVer.Git.MSBuild.svg)](https://www.nuget.org/packages/SemVer.Git.MSBuild)
+
+```powershell
+PM> Install-Package -Id SemVer.Git.MSBuild
+```
+
+[![](https://img.shields.io/nuget/v/SemVer.Svn.MSBuild.svg)](https://www.nuget.org/packages/SemVer.Svn.MSBuild)
+
+```powershell
+PM> Install-Package -Id SemVer.Svn.MSBuild
+```
+
+## Configuration
+
+You can override following properties with `Directory.Build.props`:
+
+- `SemVerStamp_BreakingChangeFormat` (default: `^perf(\(.*\))*: `)
+- `SemVerStamp_FeatureFormat` (default: `^feat(\(.*\))*: `)
+- `SemVerStamp_PatchFormat` (default: `^fix(\(.*\))*: `)
+- `SemVerStamp_BaseRevision`
+- `SemVerStamp_BaseVersion` (default: `0.0.0`)
+- `SemVerStamp_SourcePath` (default: `$(MSBuildProjectDirectory)`)
+- `SemVerStamp_Active` (default: `true` on release builds, otherwise `false`)
+
+## Developing & Building
+
+```cmd
+> git clone https://github.com/dittodhole/dotnet-SemVer.Stamp.git
+> cd dotnet-SemVer.Stamp
+dotnet-SemVer.Stamp> cd build
+dotnet-SemVer.Stamp/build> build.bat
+```
+
+This will create the following artifacts:
+
+- `artifacts/SemVer.Git.MSBuild.{version}.nupkg`
+- `artifacts/SemVer.Svn.MSBuild.{version}.nupkg`
+
+## License
+
+dotnet-SemVer.Stamp is published under [WTFNMFPLv3](https://github.com/dittodhole/WTFNMFPLv3).
+
+## Icon
+
+[Cyclops](https://thenounproject.com/term/cyclops/60203/) by [Mike Hince](https://thenounproject.com/zer0mike) from the Noun Project.
---
+++ src/SemVer.MSBuild/GetVersionBase.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using JetBrains.Annotations;
+
+namespace SemVer.MSBuild
+{
+  public abstract class GetVersionBase : Task
+  {
+    /// <remarks>TODO: rename</remarks>
+    [CanBeNull]
+    public string BaseRevision { get; set; }
+
+    /// <remarks>TODO: rename</remarks>
+    [CanBeNull]
+    public string BaseVersion { get; set; }
+
+    [Required]
+    [NotNull]
+    public string BreakingChangeFormat { get; set; }
+
+    [Required]
+    [NotNull]
+    public string FeatureFormat { get; set; }
+
+    [Output]
+    public string Version { get; set; }
+
+    [Required]
+    [NotNull]
+    public string PatchFormat { get; set; }
+
+    [Required]
+    [NotNull]
+    public string SourcePath { get; set; }
+
+    /// <inheritdoc />
+    public override bool Execute()
+    {
+      Version version;
+      try
+      {
+        version = this.CalculateVersion();
+      }
+      catch (Exception exception)
+      {
+        this.Log.LogErrorFromException(exception);
+
+        return false;
+      }
+
+      this.Version = version.ToString();
+
+      return true;
+    }
+
+    /// <exception cref="Exception"/>
+    [NotNull]
+    private Version CalculateVersion()
+    {
+      int breakingChange;
+      int feature;
+      int patch;
+
+      if (this.BaseVersion == null)
+      {
+        breakingChange = 0;
+        feature = 0;
+        patch = 0;
+      }
+      else
+      {
+        // TODO parse as semVer
+        Version baseline;
+        try
+        {
+          baseline = System.Version.Parse(this.BaseVersion);
+        }
+        catch (Exception exception)
+        {
+          throw new ArgumentOutOfRangeException($"Failed to parse {nameof(this.BaseVersion)} '{this.BaseVersion}'",
+                                                exception);
+        }
+
+        breakingChange = baseline.Major;
+        feature = baseline.Minor;
+        patch = baseline.Build;
+      }
+
+      foreach (var commitMessage in this.GetCommitMessages())
+      {
+        var patchMatches = Regex.Matches(commitMessage,
+                                         this.PatchFormat,
+                                         RegexOptions.Multiline | RegexOptions.IgnoreCase)
+                                .OfType<Match>()
+                                .ToArray();
+        if (patchMatches.Any())
+        {
+          patch += patchMatches.Length;
+        }
+
+        var featureMatches = Regex.Matches(commitMessage,
+                                           this.FeatureFormat,
+                                           RegexOptions.Multiline | RegexOptions.IgnoreCase)
+                                  .OfType<Match>()
+                                  .ToArray();
+        if (featureMatches.Any())
+        {
+          patch = 0;
+          feature += featureMatches.Length;
+        }
+
+        var breakingChangeMatches = Regex.Matches(commitMessage,
+                                                  this.BreakingChangeFormat,
+                                                  RegexOptions.Multiline | RegexOptions.IgnoreCase)
+                                         .OfType<Match>()
+                                         .ToArray();
+        if (breakingChangeMatches.Any())
+        {
+          patch = 0;
+          feature = 0;
+          breakingChange += breakingChangeMatches.Length;
+        }
+      }
+
+      var result = new Version(breakingChange,
+                               feature,
+                               patch);
+
+      return result;
+    }
+
+    /// <exception cref="Exception"/>
+    [NotNull]
+    [ItemNotNull]
+    protected abstract string[] GetCommitMessages();
+  }
+}
